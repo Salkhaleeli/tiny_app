d686c026ac75beb4e33c68147feb105f04a38ffc
salkhaleeli <saad@Saads-MBP.hitronhub.home>
2022-07-13 13:45:51 -0600

    adding func generate rand digit url and redirect to longURL

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -22,10 +25,30 @@ app.get('/urls', (req, res)=>{
   res.render('urls_index', templateVars);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL; 
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longUrl = urlDatabase[req.params.shortURL];
+  res.redirect(longUrl);
+});
+
 app.get('/urls/:shortURL',(req, res) =>{
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars)
 });
+
+app.post('/urls', (req, res)=> {
+  console.log(req.body);
+  res.send('OK')
+})
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -34,3 +57,7 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const generateRandomString = function() {
+  return Math.random().toString(36).substring(2, 6)
+}
