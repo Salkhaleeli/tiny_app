5f21080e2a400380a23cd10fa45c847f5fcc4ff8
salkhaleeli <saad@Saads-MBP.hitronhub.home>
2022-07-13 19:29:25 -0600

    adding login and fixing header

--- express_server.js
+++ express_server.js
@@ -21,7 +21,16 @@ const urlDatabase = {
 };
 const users = {};
 
-// routs
+// ROUTS
+//   |
+//   |
+//   |
+//   |
+// GET
+
+app.get('/login', (req,res) =>{
+  res.render('login');
+});
 
 app.get('/register', (req, res)=>{
   res.render('registration')
@@ -39,7 +48,7 @@ app.get("/urls/new", (req, res) => {
   const templateVars = {
     user : users[req.cookies["user_id"]]
   };
-  res.render("urls_new");
+  res.render("urls_new", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -56,8 +65,10 @@ app.get('/urls/:shortURL',(req, res) =>{
   res.render('urls_show', templateVars);
 });
 
+//Post
+
 app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
+  const shortURL = generateRandomString(6);
   urlDatabase[shortURL] = req.body.longURL; 
   res.redirect(`/urls/${shortURL}`);
 });
@@ -69,13 +80,16 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 
 app.post("/urls/:shortURL/modify", (req, res) => {
   const url = req.params.shortURL;
+  // console.log('req.body', req.body);
   const newUrl = req.body.URL;
   urlDatabase[url] = newUrl;
   res.redirect('/urls');
   }); 
 
 app.post('/login', (req, res)=>{
+  // console.log('req.body', req.body);
   res.cookie('user_id', req.body.username);
+
   res.redirect('/urls');
 })
 
@@ -85,22 +99,51 @@ app.post('/logout', (req, res)=>{
 })
 
 app.post('/register', (req, res)=> {
-  const id = generateRandomString();
+  const email = req.body.email;
+  const password = req.body.password;
+  if (!email|| !password) {
+    res.status(400);
+    res.send('Invalid Email or Password');
+  }else if (emailLookup(email)) {
+    res.status(400);
+    res.send('Username already exists.. please try forget password which i havn\'t implement')
+  }else{
+  const id = generateRandomString(6);
+  console.log('req.body', req.body);
   users[id] = {
     id :id,
     email: req.body.email,
     password: req.body.password
   }
   res.cookie('user_id', id);
-  res.redirect('/urls')
+  res.redirect('/register')
+  }
 })
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`app listening on port ${PORT}!`);
 });
 
 //functions:
 
-const generateRandomString = function() {
-  return Math.random().toString(36).substring(2, 6)
-}
+const generateRandomString = function(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+};
+
+const emailLookup = function(email) {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return true;
+    }else{
+      return false
+    }
+      
+    }
+  };
